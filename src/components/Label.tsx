ee86b2c49d83b1df36f1ff5195dc8973ade26df8
Natalia <natalialexandra015@gmail.com>
2024-09-07 20:35:05 +0200

    label

---
+++ src/components/Label.tsx
@@ -0,0 +1,7 @@
+export default function Label() {
+    return (
+        <div className="label flex-row-center">
+            <p>enjoy natural and chemical free therapeutic treatments</p>
+        </div>
+    )
+}
