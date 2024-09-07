87eb7f8260fd1efca316830305c63c7164acf476
Natalia <natalialexandra015@gmail.com>
2024-09-07 19:30:59 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div className='app'>
+        app
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
