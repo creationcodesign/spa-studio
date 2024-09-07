2c42867bf6cbba16ec514fd06bc40c08b916fcbf
Natalia <natalialexandra015@gmail.com>
2024-09-07 22:12:01 +0200

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
+import Hero from '../components/Hero'
+import Massages from '../components/Massages'
+
 export default function Home() {
     return (
-        <div>Home</div>
+        <div className="home home-page">
+            <Hero />
+            <Massages />
+        </div>
     )
 }
