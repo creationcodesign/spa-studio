d253cad79c895b55f589e4980314a1107e1d7681
Natalia <natalialexandra015@gmail.com>
2024-09-08 20:12:46 +0200

    therapist data

---
+++ src/data/therapists.ts
@@ -0,0 +1,36 @@
+const therapists = [
+    {
+        id: 1,
+        name: 'Laura Smith',
+        title: 'our therapist',
+        titleLong: 'massagist, espacialy for face',
+        experince: '5 years of experience',
+        img: 'therapist-1.png',
+    },
+    {
+        id: 2,
+        name: 'Richardo Vador',
+        title: 'our therapist',
+        titleLong: 'massagist, espacialy for face',
+        experince: '8 years of experience',
+        img: 'Therapist-2.png',
+    },
+    {
+        id: 3,
+        name: 'Anna Lee',
+        title: 'our therapist',
+        titleLong: 'massagist, espacialy for face',
+        experince: '12 years of experience',
+        img: 'Therapist-3.png',
+    },
+    {
+        id: 4,
+        name: 'John May',
+        title: 'our therapist',
+        titleLong: 'massagist, espacialy for face',
+        experince: '3 years of experience',
+        img: 'Therapist-4.png',
+    }
+]
+
+export default therapists
