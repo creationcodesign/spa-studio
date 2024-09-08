0c5572d4cc2c5175b4445e2a6f88270ad3eed9de
Natalia <natalialexandra015@gmail.com>
2024-09-08 20:47:59 +0200

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './AppRoutes'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <BrowserRouter>
       <div className='app'>
+        <Header />
         <AppRoutes />
+        <Footer />
       </div>
     </BrowserRouter>
   )
