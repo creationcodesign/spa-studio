e6e81532234a77de5de8683dd85142d3cd80e556
Natalia <natalialexandra015@gmail.com>
2024-09-08 19:28:30 +0200

    add more sections

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import Hero from '../components/Hero'
 import Massages from '../components/Massages'
+import Offer from '../components/Offer'
+import OfferSpecial from '../components/OfferSpecial'
+import Therapists from '../components/Therapists'
 
 export default function Home() {
     return (
         <div className="home home-page">
             <Hero />
             <Massages />
+            <Therapists />
+            <Offer />
+            <OfferSpecial />
         </div>
     )
 }
