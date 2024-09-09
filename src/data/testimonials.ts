57c97af495df4c99a6f4c49973e9b19af0cd1c69
Natalia <natalialexandra015@gmail.com>
2024-09-09 10:07:59 +0200

    testimonials data

---
+++ src/data/testimonials.ts
@@ -0,0 +1,40 @@
+const testimonials = [
+    {
+        id: 1,
+        name: 'Belinda',
+        title: 'Very recommendable',
+        discription: 'Anna was very nice and they all gave us a very good service. The place was clean and Aimee was really good and nice too.',
+        stars: 5,
+        date: '2 weeks ago',
+        img: 'testimonial-1.png',
+    },
+    {
+        id: 2,
+        name: 'Sofia',
+        title: "They're the best!",
+        discription: 'very nice Atmosphere very relaxing and amazing Massage',
+        stars: 5,
+        date: '8 months ago',
+        img: 'testimonial-2.png',
+    },
+    {
+        id: 3,
+        name: 'Marissa',
+        title: 'Professional massage',
+        discription: 'I am very satisfied with their massage and services and decided to book  our next appointment. I felt good as I have stiffness on my shoulder.',
+        stars: 4,
+        date: '2 months ago',
+        img: 'testimonial-3.png',
+    },
+    {
+        id: 4,
+        name: 'Alex P.',
+        title: 'Lot of pressure',
+        discription: 'Ricardo used elbows and a lot of pressure, fixed my back better than it has felt in a long time.',
+        stars: 5,
+        date: '3 months ago',
+        img: 'testimonial-4.png',
+    },
+]
+
+export default testimonials
