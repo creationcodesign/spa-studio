c84aa51d7c6d3ed9b6e49c669455dfc9f319d8da
Natalia <natalialexandra015@gmail.com>
2024-09-10 11:05:02 +0200

    services data

---
+++ src/data/services.ts
@@ -0,0 +1,44 @@
+const servises = [
+    {
+        id: 1,
+        title: 'Full Body Massage',
+        img: 'service-1.png',
+    },
+    {
+        id: 2,
+        title: 'Head or Face Massage',
+        img: 'service-2.png',
+    },
+    {
+        id: 3,
+        title: 'Hot Stones Massage',
+        img: 'service-3.png',
+    },
+    {
+        id: 4,
+        title: 'Deep Tissue Massage',
+        img: 'service-4.png',
+    },
+    {
+        id: 5,
+        title: 'Hot Oil Massage',
+        img: 'service-5.png',
+    },
+    {
+        id: 6,
+        title: 'Aromatherapye',
+        img: 'service-6.png',
+    },
+    {
+        id: 7,
+        title: 'Foot Massage ',
+        img: 'service-7.png',
+    },
+    {
+        id: 8,
+        title: 'Sea Salt Scrub & 30 Minute Massage',
+        img: 'service-8.png',
+    }
+]
+
+export default servises
