3f7ddb242b277cd53a93e9cdf7b080ba099111e0
Natalia <natalialexandra015@gmail.com>
2024-09-10 11:07:57 +0200

    therapists

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Therapists from './pages/Therapists'
 import Services from './pages/Services'
 
 export default function AppRoutes() {
@@ -9,6 +10,7 @@ export default function AppRoutes() {
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
             <Route path='/services' element={<Services />} />
+            <Route path='/therapists' element={<Therapists />} />
             <Route path='*' element={<Home />} />
         </Routes>
     )
