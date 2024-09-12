da45d8dc0d7f7a83c36e452f16be58131ff7fe20
Natalia <natalialexandra015@gmail.com>
2024-09-12 10:58:26 +0200

    add interface to send data

---
+++ src/interface/IDataToSend.ts
@@ -0,0 +1,10 @@
+export type IDataToSend = {
+    name: string
+    email: string
+    phone: string
+    service: string
+    therapist: string
+    date: string
+    time: string
+    confirmation: boolean
+}
