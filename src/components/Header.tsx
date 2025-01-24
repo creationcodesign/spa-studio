dbabf9c33acc3a6dbe2581c7c02b368ee0f0a88f
on <natalia8creation@gmail.com>
2025-01-24 23:24:59 +0100

    adjust header and navbar for responsive design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,42 @@
-import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
 import Navbar from './Navbar'
 import { navigateTo } from '../helper/navigation'
 
 export default function Header() {
+    const [isActive, setIsActive] = useState(false)
+    const toggleActive = () => setIsActive(!isActive)
+    const closeNavbar = () => setIsActive(false);
+
     return (
         <header>
-            <NavLink to='/' className='logo-link'>spa</NavLink>
-            <div className='header-right'>
-                <Navbar />
-                <button className='header-btn btn-primary' onClick={() => navigateTo('/appointment')}>
-                    <span>book now</span>
-                </button>
-                <div className="header-info">
-                    <p className='small-text'>Mo - Su 8am - 8pm</p>
-                    <p>0826 9328 2800</p>
+            <div className='header-logo-container'>
+                <NavLink to='/' className='logo-link'>spa</NavLink>
+                <div className="mobile-icons">
+                    {!isActive ?
+                        <svg width="35" height="35" viewBox="0 0 61 35" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={toggleActive}>
+                            <path d="M3 2.5H58" stroke="#BAC67F" strokeWidth="5" strokeLinecap="round" />
+                            <path d="M3 17.5H58" stroke="#BAC67F" strokeWidth="5" strokeLinecap="round" />
+                            <path d="M3 32.5H58" stroke="#BAC67F" strokeWidth="5" strokeLinecap="round" />
+                        </svg>
+                        :
+                        <svg width="35" height="35" viewBox="0 0 54 45" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={toggleActive}>
+                            <path d="M7.80371 41.8909L46.6946 3" stroke="#BAC67F" strokeWidth="5" strokeLinecap="round" />
+                            <path d="M3 14L51.4974 42" stroke="#BAC67F" strokeWidth="5" strokeLinecap="round" />
+                        </svg>
+                    }
+                </div>
+            </div>
+            <div className={`header-navbar-container ${isActive ? 'active' : ''}`}>
+                <Navbar closeNavbar={closeNavbar} />
+                <div className='header-info-container'>
+                    <button className='header-btn btn-primary btn-book' onClick={() => navigateTo('/appointment')}>
+                        <Link to="/appointment">book now</Link>
+                    </button>
+                    <div className="header-info">
+                        <p className='small-text'>Mo - Su 8am - 8pm</p>
+                        <p>0826 9328 2800</p>
+                    </div>
                 </div>
             </div>
         </header>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 import { NavLink } from 'react-router-dom'
 
-export default function Navbar() {
+export default function Navbar({ closeNavbar }: { closeNavbar: () => void }) {
     return (
         <nav>
-            <NavLink to='/services'>services</NavLink>
-            <NavLink to='/therapists'>therapists</NavLink>
-            <NavLink to='/about'>about</NavLink>
-            <NavLink to='/contact'>contact</NavLink>
+            <NavLink to='/services' onClick={closeNavbar}>services</NavLink>
+            <NavLink to='/therapists' onClick={closeNavbar}>therapists</NavLink>
+            <NavLink to='/about' onClick={closeNavbar}>about</NavLink>
+            <NavLink to='/contact' onClick={closeNavbar}>contact</NavLink>
         </nav>
     )
 }
